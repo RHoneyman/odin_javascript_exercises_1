fbb8520a896a306489bff43048d537dd3e5cfad9
Ross Honeyman <rfhoneyman@gmail.com>
2019-12-22 10:56:21 -0500

    First commit from Ross

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, repeatCount) {
+    let result = string;
 
+    if (repeatCount < 0) {
+        return 'ERROR';
+    } else if (repeatCount == 0) {
+        return '';
+    }
+
+    for (let i = 1; i < repeatCount; i++) {
+        result += string;
+    }
+
+    return result;
 }
 
 module.exports = repeatString
