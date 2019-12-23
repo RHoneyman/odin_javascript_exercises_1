6f88196de357c6cf080d4802f9ecbe2eac275e76
Ross Honeyman <rfhoneyman@gmail.com>
2019-12-23 15:07:55 -0500

    Complete sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (typeof x != "number" || typeof y != "number") {
+        return "ERROR";
+    } else if (x < 0 || y < 0) {
+        return "ERROR";
+    }
+    
+    let larger = x > y ? x : y;
+    let smaller = x < y ? x : y;
+    let sum = 0;
+
+    for (let i = smaller; i <= larger; i++) {
+        sum += i;
+    }
+
+    return sum;
+
 
 }
 
