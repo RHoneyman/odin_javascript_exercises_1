d8d80b553be61e7b3ac66e91c5fec01d9fa7a36d
Ross Honeyman <rfhoneyman@gmail.com>
2019-12-23 15:23:11 -0500

    Complete leapYears and tempConversion exercises

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    // if year is divisible by 100, it must be divisible by 400 to be a leap year.
+    // otherwise if year is divisible by 4, it is a leap year.
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 
 }
 
--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let convertedTemp = (temp - 32) * (5/9);
+  if (convertedTemp != 0) {
+    convertedTemp = convertedTemp.toFixed(1);
+  }
+  return Number(convertedTemp);
 }
 
-const ctof = function() {
+const ctof = function(temp) {
+  let convertedTemp = (temp * 9 / 5) + 32;
 
+  if (convertedTemp != 0) {
+    convertedTemp = convertedTemp.toFixed(1);
+  }
+  return Number(convertedTemp);
 }
 
 module.exports = {
