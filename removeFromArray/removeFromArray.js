ff28dbfadf928e497b8531628d228d385634e7b2
Ross Honeyman <rfhoneyman@gmail.com>
2019-12-23 14:58:59 -0500

    Complete removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
 const removeFromArray = function() {
+    let args = [...arguments];
+    let sourceArray = args[0];
+    let newArray = [];
+    let remainingArgs = args.slice(1);
+
+    for (let i = 0, n = sourceArray.length; i < n; i++) {
+        if (!remainingArgs.includes(sourceArray[i])) {
+            newArray.push(sourceArray[i]);
+        }
+    }
+
+    return newArray;
 
 }
 
